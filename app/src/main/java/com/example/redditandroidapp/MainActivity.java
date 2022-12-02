1cf0677b5daa9582243b1c195d9177b9e87d4edc
Old-songs-lo-fi <vakulacomp3@gmail.com>
2022-12-02 17:33:35 +0300

    Added connection

--- app/src/main/java/com/example/redditandroidapp/MainActivity.java
+++ app/src/main/java/com/example/redditandroidapp/MainActivity.java
@@ -2,27 +2,118 @@ package com.example.redditandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button refresh;
     private TextView slot;
+    private String url;
+    private ImageView slot_image;
+    private LinearLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Define of all future variables
+        url = "https://www.reddit.com/top.json";
         refresh = findViewById(R.id.refresh);
-        slot = findViewById(R.id.slot);
+        slot = findViewById(R.id.slot_text);
+        slot_image = findViewById(R.id.slot_image);
+
+
+        //Refresh Button active
         refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, R.string.refresh, Toast.LENGTH_SHORT).show();
+                new GetDataFromURL().execute(url);
             }
         });
     }
+
+    private class GetDataFromURL extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            slot.setText("Loading...");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader bufferedReader = null;
+
+            try {
+                //Scan Data from internet connection
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = bufferedReader.readLine()) != null){
+                    buffer.append(line).append("\n");
+                }
+                return buffer.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(connection != null){
+                    connection.disconnect();
+                }
+                if(bufferedReader != null){
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject object = new JSONObject(s);
+                Picasso.get()
+                        .load(object.getJSONObject("data").getJSONArray("children").getJSONObject(0).getJSONObject("data").getString("url_overridden_by_dest"))
+                                .into(slot_image);
+                slot.setText("Header: " + object.getJSONObject("data").getJSONArray("children").getJSONObject(0).getJSONObject("data").getString("title") +  "\n");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
+
